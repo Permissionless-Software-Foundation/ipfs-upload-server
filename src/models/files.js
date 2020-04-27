5520235c454b565c2e4556d644f0e2ae88467077
Chris Troutner <chris.troutner@gmail.com>
2020-04-26 18:58:40 -0700

    fix(file model): Adding additional properties

--- src/models/files.js
+++ src/models/files.js
@@ -6,7 +6,11 @@ const File = new mongoose.Schema({
   size: { type: Number, required: true }, // size of the file in bytes
   payloadLink: { type: String }, // IPFS hash of current file.
   txId: { type: String }, // Memo transaction Id
-  meta: { type: Object }
+  meta: { type: Object },
+  bchAddr: { type: String }, // BCH address assigned to this file.
+  hasBeenPaid: { type: Boolean, default: false }, // Flag if hosting cost has been paid.
+  pinExpires: { type: String }, // ISO date when IPFS pin for hosted content will expire.
+  hostingCost: { type: Number } // Value in satoshis of the hosting cost for this file.
 })
 
 module.exports = mongoose.model('file', File)
