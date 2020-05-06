9f3ceede5538e463ea557f128fd989a910eb680d
danielhumgon <danielhumgon@gmail.com>
2020-05-06 05:18:22 -0400

    fix(test): Fixing unit tests errors

--- test/a03-files.spec.js
+++ test/a03-files.spec.js
@@ -17,6 +17,20 @@ const LOCALHOST = `http://localhost:${config.port}`
 
 const context = {}
 
+const BCHJSLIB = require('../src/lib/bch')
+const bchjsLib = new BCHJSLIB()
+
+const WALLET_NAME = 'wallet-test'
+const WALLET_PATH = `${__dirname}/../config/${WALLET_NAME}`
+
+const createWallet =async () =>{
+  try {
+    await bchjsLib.createWallet(WALLET_PATH)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 describe('#Files', async function () {
   let sandbox
 
@@ -34,6 +48,8 @@ describe('#Files', async function () {
     const adminJWT = await testUtils.getAdminJWT()
     // console.log(`adminJWT: ${adminJWT}`)
     context.adminJWT = adminJWT
+
+    createWallet()
   })
 
   // Restore the sandbox before each test.
