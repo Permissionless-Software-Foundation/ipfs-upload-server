ad804c26dd3f47dcb5423e8a66fb904eb49989c4
Chris Troutner <chris.troutner@gmail.com>
2020-05-20 08:32:27 -0700

    fix(file upload): Fixed file upload

--- src/modules/uppy-files/controller.js
+++ src/modules/uppy-files/controller.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const tus = new TUS()
 
 // Handles the file upload.
-async function addFile(ctx) {
+async function addFile (ctx) {
   const tusServer = await tus.server()
   if (!tusServer) ctx.throw(500)
 
@@ -14,39 +14,38 @@ async function addFile(ctx) {
     console.log('Upload Completed!')
 
     try {
+      console.log(`File data: ${JSON.stringify(event.file, null, 2)}`)
+
       // Get metadata from file uploaded
       const metad = await tus.parseMetadataString(event.file.upload_metadata)
-      //console.log(`File metadata : ${JSON.stringify(metad, null, 2)}`)
+      console.log(`File metadata : ${JSON.stringify(metad, null, 2)}`)
 
       const fileName = event.file.id
+      const originalFileName = metad.filename ? metad.filename.decoded : ''
 
-      const fileModelId = metad.fileModelId ?
-        metad.fileModelId.decoded : ''
+      const fileModelId = metad.fileModelId ? metad.fileModelId.decoded : ''
 
-      // Verify that this file contains a model 
+      // Verify that this file contains a model
       // created in the database
       const file = fileModelId ? await File.findById(fileModelId) : null
-      //console.log(`File Model : ${JSON.stringify(file, null, 2)}`)
+      console.log(`File Model found: ${JSON.stringify(file, null, 2)}`)
 
       // Delete file if model doesn't exist
       if (!file) {
-        //Delete file 
+        // Delete file
         await tus.deleteFile(fileName)
-
       }
 
-      // Rename file for better reference,
-      // and to be able to use them in the future 
-      //fs.renameSync(
-      //  `uppy-files/${fileName}`,
-      //  `uppy-files${metad.fileNameToEncrypt.decoded}`
-      //)
-
-
+      // Rename file so it matches the users original file name.
+      if (originalFileName) {
+        fs.renameSync(
+          `uppy-files/${fileName}`,
+          `uppy-files/${originalFileName}`
+        )
+      }
     } catch (error) {
       console.log(error)
     }
-
   })
 
   return tusServer.handle(ctx.req, ctx.res)
